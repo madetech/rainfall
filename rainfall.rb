1d367ddf89d4d4a0f6c7ab7808f1ffe2f2c964f1
Richard Foster <richard@madetech.co.uk>
2016-08-05 12:21:01 +0100

    Matthew Blewitt - 05/08/2016

--- rainfall.rb
+++ rainfall.rb
@@ -1,5 +1,27 @@
 class Rainfall
   def calculate(heights)
-    :whatever
+    return 0 if heights.size == 2
+    if heights.size == 3
+      left_wall = heights.first
+      right_wall = heights.last
+
+      left_wall_index = heights.index(left_wall)
+      right_wall_index = heights.rindex(right_wall)
+
+      calculate_puddle(left_wall, right_wall, left_wall_index, right_wall_index, heights)
+    end
+  end
+
+  def calculate_puddle(left_wall, right_wall, left_wall_index, right_wall_index, heights)
+    length = (right_wall_index) - (left_wall_index)
+    puts length
+    highest = [left_wall, right_wall].max
+    puts highest
+
+    raw_volume = length * highest
+    puts raw_volume
+    rainfall = heights[(left_wall_index + 1)..(right_wall_index -1)].reduce(:+)
+
+    raw_volume - rainfall
   end
 end

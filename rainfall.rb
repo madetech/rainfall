df43fbab87e5385d02faa6c49979c8ea43bf827d
Richard Foster <richard@madetech.co.uk>
2016-12-21 15:44:19 +0000

    Igor de Alcantara Barroso

--- rainfall.rb
+++ rainfall.rb
@@ -1,5 +1,19 @@
 class Rainfall
   def calculate(heights)
-    :whatever
+    volumes = []
+    heights.each_with_index do |height, i|
+      left_max = heights[0..i].max
+      right_max = heights[i..-1].max
+
+      top = [left_max, right_max].min
+
+      if top > height && i != 0 && i != (heights.length - 1)
+        volumes << top - height
+      end
+    end
+
+    volumes.reduce(0) do |sum, volume|
+      sum + volume
+    end
   end
 end

9f1cec64ec63e66ecc0709f2b827de8ae9dab0cd
Craig J. Bass <craig@madetech.co.uk>
2017-01-05 17:53:11 +0000

    Code that was written

--- rainfall.rb
+++ rainfall.rb
@@ -1,5 +1,13 @@
 class Rainfall
   def calculate(heights)
-    :whatever
+    area = 0
+
+    heights.each_with_index do |height, i|
+      if heights[i-1] > height && heights[i+1] > height
+        area += 1 unless heights[i-1].nil? || heights[i+1].nil?
+      end
+    end
+
+    area
   end
 end

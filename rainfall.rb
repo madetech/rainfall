fb4aee1cb3438ba9ba918cb9fedbb3bcfe857817
Richard Foster <richard@madetech.co.uk>
2017-01-11 10:28:41 +0000

    Tests green

--- rainfall.rb
+++ rainfall.rb
@@ -1,5 +1,16 @@
 class Rainfall
   def calculate(heights)
-    1
+    @total_area = 0
+
+    heights.each_with_index do |v, i|
+      max = heights[0..i].max
+      right_hand_max = heights[i..-1].max
+
+      lowest_wall = [max, right_hand_max].min
+
+      @total_area += lowest_wall - v
+    end
+
+    @total_area
   end
 end
